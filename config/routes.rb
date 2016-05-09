b489dcc3a321edd86f6ba3585f202a22afd5ce1d
BotchM <botchm@hotmail.com>
2016-05-09 04:19:42 -0700

    sigin halfway through done-botch

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   #get 'users/new'
 
-  root                 'flips#home'
-  get  'signin'     => 'flips#signin'
-  get  'signup'     => 'users#new'
-
+  root                   'flips#home'
+  get    'signup'     => 'users#new'
+  get    'signin'     => 'sessions#new'
+  post   'signin'     => 'sessions#create'
+  delete 'logout'     => 'sessions#destroy'
   #Resources
   resources :users
 end
